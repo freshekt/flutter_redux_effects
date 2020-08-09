70a6bd772ddf78452fcdb45a43f28278bfd1cf22
Ivan Anisimov <anisimov@sharp-dev.net>
2020-08-09 14:19:17 +0500

    initial lib

---
+++ lib/BaseReducer.dart
@@ -0,0 +1,18 @@
+
+
+import 'Action.dart';
+import 'StoreService.dart';
+
+abstract class BaseReducer<S> {
+  dynamic execute(StoreService service, dynamic state, Action action) {
+    var selector = service.getSelector<S>();
+    if (selector != null) {
+      var cur_state = selector.get(state);
+      var newState = doAction(cur_state, action);
+      return selector.set(state, newState);
+    }
+    return doAction(state, action);
+  }
+
+  S doAction(S state, Action action);
+}
